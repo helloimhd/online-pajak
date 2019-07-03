3c58809819336ef10ec4bde3a35c67c6bac6b68e
helloimhd <herda58@gmail.com>
2019-07-03 22:05:34 +0800

    create a func for calculation instead

--- script.js
+++ script.js
@@ -31,24 +31,19 @@ const annualIncome = function(income) {
     return income * 12;
 };
 
-const inputHappened = function(currentInput) {
-    const yearlyIncome = annualIncome(parseInt(currentInput));
-    let newFigure = yearlyIncome;
-
-    let totalTax = 0;
-
-    let calculation = taxableIncome.map((obj, index) => {
-        console.log("New figure before deducting: ", newFigure)
+const calculation = function(taxableIncomeObj, currentAnnualIncome, totalTax) {
+    taxableIncome.map(obj => {
+        console.log("New figure before deducting: ", currentAnnualIncome)
         let range = obj.max - obj.min;
 
-        if (newFigure > range) {
+        if (currentAnnualIncome > range) {
             let amount = range * obj.rate;
             totalTax = amount + totalTax;
-            newFigure = newFigure - range;
+            currentAnnualIncome = currentAnnualIncome - range;
             console.log("Range: ", range);
             console.log("Amount of tax for the bracket: ", amount);
             console.log("Current total tax: ", totalTax)
-            console.log("New figure: ", newFigure)
+            console.log("New figure: ", currentAnnualIncome)
 
             console.log(`${obj.type}: ${totalTax}`)
             console.log("/////////////////////////////////")
@@ -56,13 +51,13 @@ const inputHappened = function(currentInput) {
         // if the left over figure is lesser than the range
         } else {
             // check if figure is more than 0, make sure its not negative
-            if (newFigure > 0) {
+            if (currentAnnualIncome > 0) {
                 console.log(obj.rate);
                 // amount of tax for the bracket
-                let amount = newFigure * obj.rate;
+                let amount = currentAnnualIncome * obj.rate;
                 totalTax = amount + totalTax;
                 // still need to update new figure
-                newFigure = newFigure - newFigure;
+                currentAnnualIncome = currentAnnualIncome - currentAnnualIncome;
 
 
             } else {
@@ -70,20 +65,25 @@ const inputHappened = function(currentInput) {
             }
 
             console.log("/////////////////////////////////")
-            // let amount = newFigure * obj.rate;
-            // totalTax = amount + totalTax;
 
-            // console.log("Range: ", range);
-            // console.log("Amount: ", amount);
-            // console.log("Total tax: ", totalTax)
-            // console.log("New figure: ", newFigure)
-
-            // console.log(`${obj.type}: ${totalTax}`)
-            // console.log("/////////////////////////////////")
         }
+    }) // end of map
 
-    })
+    return totalTax;
+}  // end of function
 
-    console.log("Total tax: ", totalTax);
+
+
+
+
+const inputHappened = function(currentInput) {
+    // cannot change
+    const yearlyIncome = annualIncome(parseInt(currentInput));
+    let newFigure = yearlyIncome;  // can change and play around with it
+
+    let totalTax = 0;
+
+    let newTotalTax = calculation(taxableIncome, newFigure, totalTax)
+    console.log("Total tax: ", newTotalTax);
     console.log("Yearly income: ", yearlyIncome)
 }
